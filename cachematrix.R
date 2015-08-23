01c04fdd3076baee637ed25ffe5f0fc1be851425
Ruth Lara Castelán <Ruthlaracast@gmail.com>
2015-08-23 15:00:59 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,34 +2,34 @@
 # this function works like a class, it creates a list
 
 makeCacheMatrix <- function(x = matrix()) {
-      xinv <- NULL # this is where the result of inversion is stored
+      xinverse <- NULL # this is where the result of inversion is stored
             set <- function(y) {
 	  x <<- y
-	  xinv <<- NULL 
+	  xinverse <<- NULL 
       }
       get <- function() 
-      setInv <- function(inv) xinv <<- inv # set the inversed matrix
-      getInv <- function() xinv # return the inversed matrix
+      setInverse <- function(inverse) xinverse <<- inverse # set the inversed matrix
+      getInverse <- function() xinverse # return the inversed matrix
       # return a list that contains these functions, so that we can use
       list(set = set, get = get,
-	       setInv = setInv,
-	       getInv = getInv)
+	       setInverse = setInverse,
+	       getInverse = getInverse)
   }
   cacheSolve <- function(x, ...) {
-      m <- x$getInv() # get the inversed matrix from object x
+      m <- x$getInverse() # get the inversed matrix from object x
       if(!is.null(m)) { # if the inversion result is there
-	  message("getting cached data")
+	  message("getting data cached")
 	  return(m) # return the calculated inversion
       }
       data <- x$get() 
       m <- solve(data) # we solve it
-      x$setInv(m) # then set it to the object
+      x$setInverse(m) # then set it to the object
       m # return the solved result
   }
 
   test <- matrix(runif(9,1,100),3,3)
     testCached <- makeCacheMatrix(test)
-    testInv <- cacheSolve(testCached)
+    testInverse <- cacheSolve(testCached)
   
 }
 ## THIS FUNCTION WORKS LIKE A CLASS from now on calculate or retrieve calculated inversion using the cacheSolve function.
